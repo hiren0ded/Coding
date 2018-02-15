3ea0eac7154bee41f503bfbb2e560b58f1cf5683
HIREN <HIREN@192.168.2.3>
2018-02-15 22:15:09 +0530

    intial upload

--- src/github/test.java
+++ src/github/test.java
@@ -9,14 +9,6 @@ package github;
  *
  * @author HIREN
  */
-public class Github {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("YES");
-    }
+public class test {
     
 }
